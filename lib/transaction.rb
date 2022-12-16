a92f37d3eca81e18de9868b819a1cb947af3faa0
Hogger <adagio84@btinternet.com>
2022-12-16 04:02:02 +0000

    added method to stop negative integer transactions + tests

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,17 +1,26 @@
 class Transaction
-
   def initialize(account)
     @account = account
   end
-
   def get_date
     return Time.now.strftime('%m/%d/%Y')
   end
 
-  def deposit(ammount)
+  def check_amount_valid(amount)
+    fail "amount must be a positive integer" unless amount > 0 && amount.to_f % 2 == 0
+  end
+
+  def deposit(amount)
+    check_amount_valid(amount)
     transaction = []
-    transaction << get_date << ammount
+    transaction << get_date << amount
     @account << transaction
   end
 
+  def withdraw(amount)
+    check_amount_valid(amount)
+    transaction = []
+    transaction << get_date << - amount
+    @account << transaction
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,5 @@
 require_relative '../lib/transaction'
-
 describe 'Transaction class' do
-
   context 'get_time method' do
     it 'returns the current time formatted to dd/mm/yyyy' do
       date = Transaction.new([]).get_date
@@ -9,13 +7,42 @@ describe 'Transaction class' do
       expect(date).to eq Time.now.strftime('%m/%d/%Y') # today's date
     end
   end
-
   context 'deposit method' do
-    it 'handles a deposit to the account' do
+    it 'handles a deposit transaction' do
       account = Transaction.new([])
       result = account.deposit(200)
       expect(result[0][1]).to eq 200
     end
+
+    it 'fails a non positive argument for amount' do
+      account = Transaction.new([])
+      expect { account.deposit(-200) }.to raise_error "amount must be a positive integer"
+    end
+
+    it 'fails a non integer argument for amount' do
+      account = Transaction.new([])
+      expect { account.deposit(3.5) }.to raise_error "amount must be a positive integer"
+    end
+
+  end
+
+  context 'withdraw method' do
+    it 'handles a withdraw transaction' do
+      account = Transaction.new([])
+      result = account.withdraw(200)
+      expect(result[0][1]).to eq -200
+    end
+
+    it 'fails a non positive argument for amount' do
+      account = Transaction.new([])
+      expect { account.withdraw(-200) }.to raise_error "amount must be a positive integer"
+    end
+
+    it 'fails a non integer argument for amount' do
+      account = Transaction.new([])
+      expect { account.withdraw(3.5) }.to raise_error "amount must be a positive integer"
+    end
+
   end
 
 end
